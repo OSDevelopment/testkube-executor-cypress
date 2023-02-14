c1c21c36219e2eae233767580e020aec7fd9857e
Robinson Presotto <robinson.presotto@kongsbergdigital.com>
2023-02-14 09:37:36 +0100

    Demo

--- examples/cypress/e2e/simple.cy.js
+++ examples/cypress/e2e/simple.cy.js
@@ -1,9 +1,9 @@
 describe('The Home Page', () => {
   it('successfully loads', () => {
-    cy.visit('https://testkube.kubeshop.io');
+    cy.visit('https://www.kongsberg.com');
 
     cy.contains(
-      'Testkube provides a Kubernetes-native framework for test definition, execution and results'
+      'KONGSBERG specialises in developing advanced technologies'
     );
   });
 });
